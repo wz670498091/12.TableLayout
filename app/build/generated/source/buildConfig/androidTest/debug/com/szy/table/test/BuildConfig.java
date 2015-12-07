eb0729444a0dcc9a5f44c99793507d5c3d226a2c
wangzhen <670498091@qq.com>
2015-12-07 18:06:49 +0800

    TableLayout APP 应用。

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/szy/table/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.szy.table.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.szy.table.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+}
